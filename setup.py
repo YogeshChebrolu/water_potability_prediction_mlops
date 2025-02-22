251d9cbcf3795d5611d4025de9238b14d42b92ee
Yogesh <chebroluyogesh25@gmail.com>
2025-02-22 09:48:58 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML prediction of water potability',
+    author='yogesh',
+    license='MIT',
+)
